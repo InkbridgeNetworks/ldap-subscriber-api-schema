2d6b68824b5997c207e2a673d41d95d5c280f291
arr2036 <a.cudbardb@networkradius.com>
2025-11-26 22:23:26 -0800

    Add global endpoint

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE3Njk2Mw/spectral.js?branch_slug=master&token=27c657b4-52c8-4bc5-ae91-81f4135fc2b9";
+export default { extends: ruleset };
